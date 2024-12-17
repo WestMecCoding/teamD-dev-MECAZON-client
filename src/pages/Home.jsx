17c7b9232433152cb047b9c75565f3dd69aefdfb
git <dromer114@west-mec.org>
2024-12-17 13:38:15 -0700

    removed review

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import StarRating from "../components/StarRating";
 import NewReview from "../components/NewReview";
 export default function Home() {
-  return (<>
+  return (
+    <>
     <h1>Welcome to the Shopping Website</h1>
     {/* <StarRating></StarRating> */}
     <NewReview></NewReview>
